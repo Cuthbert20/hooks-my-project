3e23ff56971ec010f26e1b31df7b25436901eec9
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-04 17:11:54 -0600

    using useState to set up state, created Todo component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>;
+}
 
 function App() {
+  //useState works with setting up state (this.state) && updating state (this.setState)
+  const [todos, setTodos] = useState([
+    //we are setting up our state which is an array of objects
+    {
+      text: "Learn about react",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build cool todo app",
+      isCompleted: false
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          //passing props to Todo Component which is a function Component that is declared above in App.js Component.
+          //so different then class based but, very exciting.
+          <Todo key={index} index={index} todo={todo}></Todo>
+        ))}
+      </div>
     </div>
   );
 }
